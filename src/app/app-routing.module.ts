dae3cb3b13b5eff3fa60f121904117f7b542e9c0
Sergey Petrochenko <mail.petrochenko@gmail.com>
2017-08-14 11:12:55 +0300

    Creating feature's modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,44 +1,10 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {RecipesComponent} from './recipes/recipes.component';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
-import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
-import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
-import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
-import {SignupComponent} from './auth/signup/signup.component';
-import {SigninComponent} from './auth/signin/signin.component';
-
-import {AuthGuardService} from './sevices/auth-guard.service';
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  {
-    path: 'recipes',
-    component: RecipesComponent,
-    children: [
-      {
-        path: '',
-        component: RecipeStartComponent
-      },
-      {
-        path: 'new',
-        component: RecipeEditComponent,
-        canActivate: [AuthGuardService]
-      },
-      {
-        path: ':id',
-        component: RecipeDetailComponent
-      },
-      {
-        path: ':id/edit',
-        component: RecipeEditComponent,
-        canActivate: [AuthGuardService]
-      }
-    ]
-  },
   {path: 'shopping-list', component: ShoppingListComponent},
-  {path: 'sign-up', component: SignupComponent},
-  {path: 'sign-in', component: SigninComponent},
 ];
 
 @NgModule({
